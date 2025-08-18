e08353cf6a9e5fa80d33d59fc01fdefce0e1716e
RJMQ1 <manuelaqporto@gmail.com>
2025-08-18 09:49:29 -0300

    Criar superturfo.c

---
+++ superturfo.c
@@ -0,0 +1,12 @@
+#include <Studio.h>
+ int mais() {
+  int carta1;
+ printf("digite o número da carta1");
+ scanf("%d", o número da carta1)
+  char estado[1];
+printf("digite representante do estado");
+scanf
+
+ 
+return 0;
+ }
